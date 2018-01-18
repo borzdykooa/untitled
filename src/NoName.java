e23bb08fd6a1877315395a29d367017fde4254ac
borzdykooa <borzdykooa@mail.ru>
2018-01-18 22:38:20 +0300

    test commit

---
+++ src/NoName.java
@@ -0,0 +1,1 @@
+public class NoName {        public static void main (String [] args){            System.out.println("Hello World");            int x=2;            int y=++x;            System.out.println(x);            System.out.println(y);        }    }
